9b0879c081a111b37c38bcdbb94186dea07d360b
KamranYsupov <kamrankek48@gmail.com>
2025-02-26 10:55:35 +0400

    админка

--- web/apps/telegram_users/admin.py
+++ web/apps/telegram_users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    TelegramUser,
+    TaxiDriver,
+    Car
+)
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaxiDriver)
+class TaxiDriverAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    pass
